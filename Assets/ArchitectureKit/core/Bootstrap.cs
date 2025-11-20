c7d9e72af1e2886810edf0665a29e83100f657dd
aditya <adityamuhammad600@gmail.com>
2025-11-20 19:54:35 +0700

    update
    update-- fix bug state

--- Assets/ArchitectureKit/core/Bootstrap.cs
+++ Assets/ArchitectureKit/core/Bootstrap.cs
@@ -1,10 +1,8 @@
-using System.Collections.Generic;
 using UnityEngine;
 using MyGameLoop;
 using MyInput;
 using MyLevel;
 using MyUI;
-using System;
 
 public sealed class Bootstrap : MonoBehaviour
 {
@@ -14,11 +12,6 @@ public sealed class Bootstrap : MonoBehaviour
     [SerializeField] private InputGroup inputManager;
 
     private IBootstrapContext context;
-    /*private List<Manager> managers = new List<Manager>();
-    private IGameLoopManager _iGameLoopManager;
-
-    private IEventBus _bus;
-    private GameState _gameState;*/
 
     private void Awake()
     {
@@ -50,58 +43,16 @@ public sealed class Bootstrap : MonoBehaviour
 
         foreach (var inst in installers)
             inst.Install(context);
-
-        /*IFactory<FactoryComponent.Args, GameObject> factory_component = new FactoryComponent();
-        IUpdateHandler updateHandler = new UpdateHandler();
-
-        _bus = new EventBus();
-        _iGameLoopManager = new GameLoopManager(updateHandler as UpdateHandler);
-        managers.Add(_iGameLoopManager);
-
-        Manager temp = new UIManager(uiManager, factory_component);
-        managers.Add(temp);
-
-        temp = new LevelManager(_bus);
-        managers.Add(temp);
-
-        temp = new InputManager(_bus, inputManager);
-        managers.Add(temp);*/
     }
 
     private void Start()
     {
         context.IGetGameState.Change(new MainMenuState(context.IGetBus));
         context.IGetBus.IPublish(new SLevelLoad("mainmenu_scene"));
-
-        /*IKeyFactory<FactoryState.EState, IState> factory_state = new FactoryState(_bus);
-
-        IUIManager ui_manager = GetManager<UIManager>();
-
-        _gameState = new GameState();
-        UIController _uiController = new UIController(_bus, ui_manager);
-        StateController _stateController = new StateController(_bus, _gameState, factory_state);
-        InputController _inputController = new InputController(_bus);
-
-        _gameState.Change(new MainMenuState(_bus));
-
-        _bus.IPublish(new SLevelLoad("mainmenu_scene"));*/
     }
 
     private void Update()
     {
         context.IGetGameLoop.IUpdate();
-        /*_iGameLoopManager.IUpdate();
-        _gameState.Update(Time.deltaTime);*/
     }
-
-    /*private T GetManager<T>() where T : Manager
-    {
-        foreach (var manager in managers)
-        {
-            if (manager is T)
-                return (T)manager;
-        }
-
-        return default;
-    }*/
 }
--- Assets/ArchitectureKit/core/installer/state/GameStateInstaller.cs
+++ Assets/ArchitectureKit/core/installer/state/GameStateInstaller.cs
@@ -3,8 +3,8 @@
     public void Install(IBootstrapContext installer)
     {
         IEventBus bus = installer.IGetBus;
+        GameState gameState = installer.IGetGameState;
         IKeyFactory<FactoryState.EState, IState> factory_state = new FactoryState(bus);
-        GameState gameState = new GameState();
         StateController _stateController = new StateController(bus, gameState, factory_state);
     }
 }
